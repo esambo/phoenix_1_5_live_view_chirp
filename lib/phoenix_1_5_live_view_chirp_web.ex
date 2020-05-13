7f99672cf2627e156de66eed5ee2515de6de03ef
Emmanuel Sambo <news@EmmanuelSambo.com>
2020-05-13 16:48:52 -0500

    mix phx.gen.live Timeline Post posts username body:string likes_count:integer reposts_count:integer

--- lib/phoenix_1_5_live_view_chirp_web.ex
+++ lib/phoenix_1_5_live_view_chirp_web.ex
@@ -82,6 +82,7 @@ defmodule Phoenix15LiveViewChirpWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import Phoenix15LiveViewChirpWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
