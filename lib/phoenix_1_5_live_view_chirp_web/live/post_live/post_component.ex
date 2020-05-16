8d46f04bb2e0e4df234fa5dc5f03a6c1c3adb68f
Emmanuel Sambo <news@EmmanuelSambo.com>
2020-05-16 11:28:15 -0500

    Use the assigns variable in the template

--- lib/phoenix_1_5_live_view_chirp_web/live/post_live/post_component.ex
+++ lib/phoenix_1_5_live_view_chirp_web/live/post_live/post_component.ex
@@ -3,16 +3,16 @@ defmodule Phoenix15LiveViewChirpWeb.PostLive.PostComponent do
 
   def render(assigns) do
     ~L"""
-    <tr id="post-<%= post.id %>">
-      <td><%= post.username %></td>
-      <td><%= post.body %></td>
-      <td><%= post.likes_count %></td>
-      <td><%= post.reposts_count %></td>
+    <tr id="post-<%= @post.id %>">
+      <td><%= @post.username %></td>
+      <td><%= @post.body %></td>
+      <td><%= @post.likes_count %></td>
+      <td><%= @post.reposts_count %></td>
 
       <td>
-        <span><%= live_redirect "Show", to: Routes.post_show_path(@socket, :show, post) %></span>
-        <span><%= live_patch "Edit", to: Routes.post_index_path(@socket, :edit, post) %></span>
-        <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: post.id, data: [confirm: "Are you sure?"] %></span>
+        <span><%= live_redirect "Show", to: Routes.post_show_path(@socket, :show, @post) %></span>
+        <span><%= live_patch "Edit", to: Routes.post_index_path(@socket, :edit, @post) %></span>
+        <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] %></span>
       </td>
     </tr>
     """
