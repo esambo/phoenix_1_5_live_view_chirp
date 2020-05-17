941feee1a9d3545847db443450574bd7073bf675
Emmanuel Sambo <news@EmmanuelSambo.com>
2020-05-16 19:08:08 -0500

    Use CSS flexbox layout

--- lib/phoenix_1_5_live_view_chirp_web/live/post_live/post_component.ex
+++ lib/phoenix_1_5_live_view_chirp_web/live/post_live/post_component.ex
@@ -3,18 +3,37 @@ defmodule Phoenix15LiveViewChirpWeb.PostLive.PostComponent do
 
   def render(assigns) do
     ~L"""
-    <tr id="post-<%= @post.id %>">
-      <td><%= @post.username %></td>
-      <td><%= @post.body %></td>
-      <td><%= @post.likes_count %></td>
-      <td><%= @post.reposts_count %></td>
+    <div id="post-<%= @post.id %>" class="post">
+      <div class="row">
+        <div class="column column-10">
+          <div class="post-avatar"></div>
+        </div>
+        <div class="column column-90 post-body">
+          <b>@<%= @post.username %></b>
+          <br/>
+          <%= @post.body %>
+        </div>
+      </div>
 
-      <td>
-        <span><%= live_redirect "Show", to: Routes.post_show_path(@socket, :show, @post) %></span>
-        <span><%= live_patch "Edit", to: Routes.post_index_path(@socket, :edit, @post) %></span>
-        <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] %></span>
-      </td>
-    </tr>
+      <div class="row">
+        <div class="column">
+          <i class="far fa-heart"></i>
+          <%= @post.likes_count %>
+        </div>
+        <div class="column">
+          <i class="far fa-retweet"></i>
+          <%= @post.reposts_count %>
+        </div>
+        <div class="column">
+          <%= live_patch to: Routes.post_index_path(@socket, :edit, @post) do %>
+            <i class="far fa-edit"></i>
+          <% end %>
+          <%= link to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] do %>
+            <i class="far fa-trash-alt"></i>
+          <% end %>
+        </div>
+      </div>
+    </div>
     """
   end
 end
